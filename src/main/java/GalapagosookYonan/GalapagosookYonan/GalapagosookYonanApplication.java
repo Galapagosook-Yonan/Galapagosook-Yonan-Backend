c5be7767e95dc8693b9471adb145816b11884d7a
ajeong <ajung7038@naver.com>
2024-03-10 00:23:11 +0900

    Feat: 초기 세팅

---
+++ src/main/java/GalapagosookYonan/GalapagosookYonan/GalapagosookYonanApplication.java
@@ -0,0 +1,13 @@
+package GalapagosookYonan.GalapagosookYonan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalapagosookYonanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalapagosookYonanApplication.class, args);
+	}
+
+}
